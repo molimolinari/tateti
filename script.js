096e44e29d5099c4bd13d60f9d39eca259ef51ae
molimolinari <pablofmoli@gmail.com>
2020-04-23 20:15:35 -0300

    creacion de marcas en el tablero, 0 o X

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var origBoard;
-const huPlayer = '0';
-const aiPlayer = 'X';
-const winCombos = [
+var origBoard; //debe ser el tablero vacio
+const huPlayer = '0'; //human player
+const aiPlayer = 'X'; //AI player
+const winCombos = [ //combos ganadores
 	[0, 1, 2],
 	[3, 4, 5],
 	[6, 7, 8],
@@ -12,20 +12,57 @@ const winCombos = [
 	[6, 4, 2],
 ]
 
-const cells = document.querySelectorAll('.cell') //elige todos los elementos HTML q tengan la clase cell
+const cells = document.querySelectorAll('.cell') //elije todos los elementos HTML q tengan la clase cell y los mete en un objeto
 startGame();
 console.log('afuera de la funcion');
 function startGame() {
 	console.log('startgame');
-	document.querySelector(".endgame").style.display = "none";
-	origBoard = Array.from(Array(9).keys()); //crea un arreglo del 0 al 9, con el metodo array.from
+	document.querySelector(".endgame").style.display = "none"; //oculta la clase endgame, en caso de que hayas jugado antes
+	origBoard = Array.from(Array(9).keys()); //crea un arreglo del 0 al 8, con el metodo array.from
 	for (var i = 0; i < cells.length; i++) {
-		cells[i].innerText = '';
-		cells[i].style.removeProperty('background-color');
-		cells[i].addEventListener('click', turnClick, false); //agrego un event listener en cada celda y ejecuto la funcion turnclick
+		cells[i].innerText = ''; //limpia la tabla
+		cells[i].style.removeProperty('background-color'); //sacale color de fondo
+		cells[i].addEventListener('click', turnClick, false); 
+		//agrego un event listener, q queda cargado, en cada celda y ejecuto la funcion turnclick
 	}
 }
 
-function turnClick(square) {
-	console.log(square.target.id)
+function turnClick(square) { //square es el td en este caso. Turnclick le pasa el objeto clickeado como parametro. Square es el objeto q tiene las propuedades del td
+	//console.log(square.target.id)
+	turn(square.target.id, huPlayer)
 }
+
+function turn(squareId, player){
+	//le pasa el simbolo del jugador al cuadradito de la tabla
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player; //para plancharlo en lo q se ve, q es el html.
+	let gameWon = checkWin(origBoard, player);
+	if (gameWon) gameOver(gameWon);
+}
+
+function checkWin(board, player) { //board es la info dle tablero actual y player es el q hizo la ultima jugada
+	let plays = board.reduce((a,e,i) => (e === player)) ? a.concat(i) : a, []); //repasar arrow functions y ternary operator y q hace el metodo "reduce"
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem > -1)) {
+			gameWon = {index: index, player: player};
+		break;
+		})
+	}
+	return gameWon;
+
+}
+
+/*
+Arrow Function const add = (a, b) => (a + b)  
+Is equivalent to  const add = (a, b) => {     return a + b; }  
+When you use the() after your => it just automatically returns the values inside.  
+const moli = (x, y) => (x+y);  console.log(moli(1,2) )
+
+ternary operator:
+"La Cuota es de:  " + (isMember ? "$2.00" : "$10.00")
+
+
+
+*/
+
